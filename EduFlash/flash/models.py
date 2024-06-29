9f213502aa5e43cbdb87788d4a2fb7617a175c6a
Samfrodo <adeyemifrodo@gmail.com>
2024-06-29 23:22:52 +0100

    Resource Model created

--- EduFlash/flash/models.py
+++ EduFlash/flash/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import uuid
+
 
 # Create your models here.
+
+class Resource(models.Model):
+    '''the resource class'''
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    filename = models.CharField(max_length=200)
+    filepath = models.FileField(upload_to='files/', verbose_name='')
+    user = models.ForeignKey(User, null=True , default=None, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.filename
