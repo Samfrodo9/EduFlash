fd367404532ecc2435997007ab7193d670f418ef
Samfrodo <adeyemifrodo@gmail.com>
2024-06-29 22:08:56 +0100

    Settings updated

--- EduFlash/EduFlash/settings.py
+++ EduFlash/EduFlash/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from os import getenv, path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'EduFlash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+			BASE_DIR / 'templates'
+		],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT= path.join(BASE_DIR, 'media/')
+MEDIA_URL= "/media/"
+
+LOGIN_REDIRECT_URL = "/"
