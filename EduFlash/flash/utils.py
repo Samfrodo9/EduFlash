a14b0ba98fd819a328b967a8ba7190457f58829a
Samfrodo <adeyemifrodo@gmail.com>
2024-06-30 00:51:01 +0100

    Created logic for text processing, cleaning and flashcard creation

---
+++ EduFlash/flash/utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import requests
+from os import getenv
+import re
+"""Logic for text processing, flashcard creation"""
+
+API_TOKEN = getenv("HUGGINGFACE_API_TOKEN")
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+instructions = "generate question(Q) and answer(A) pair from the text above"
+
+def main(text):
+    text = text.replace('\n', ' ')
+    text = text.replace('  ', ' ')
+    output = query({
+        'inputs': f"{text}. {instructions}",
+        })
+    print(output)
+    questions = re.findall("\sQ:(.*)\?", output[0]['generated_text'])
+    answers = re.findall("\sA:(.*)\.",  output[0]['generated_text'])
+    dict_ = {}
+    for i in range(len(questions)):
+        try:
+            dict_[questions[i]] = answers[i]
+        except Exception:
+            pass
+    return dict_
